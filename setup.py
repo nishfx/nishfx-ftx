79c6b353ecb914e33a7ace21093349b0048b4e13
nishfx <bitmex@patrickbade.de>
2021-07-30 16:30:04 +0200

    regular

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ftx-api-wrapper-python3',
-      version='0.1',
+      version='0.2',
       description='FTX Exchange API wrapper in python3',
       url='https://github.com/LeeChunHao2000/ftx-api-wrapper-python3',
       author='Brendan C. Lee',
